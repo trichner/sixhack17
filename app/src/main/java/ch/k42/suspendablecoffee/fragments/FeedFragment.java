36aff33306532a26451416118f200df8daa8529e
Thomas Richner <thomasrichner@oviva.ch>
2017-03-11 22:20:33 +0100

    Some work on feed.

--- app/src/main/java/ch/k42/suspendablecoffee/fragments/FeedFragment.java
+++ app/src/main/java/ch/k42/suspendablecoffee/fragments/FeedFragment.java
@@ -1,12 +1,14 @@
 package ch.k42.suspendablecoffee.fragments;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import ch.k42.suspendablecoffee.R;
+import emoji4j.EmojiUtils;
 
 public class FeedFragment extends Fragment {
 
@@ -23,8 +25,10 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View fragment = inflater.inflate(R.layout.fragment_feed, container, false);
-        // Inflate the layout for this fragment
+        final View fragment = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        final TextView txtCard1 = (TextView) fragment.findViewById(R.id.txtCard1);
+        txtCard1.setText(EmojiUtils.emojify("Hey! Thanks a lot for the coffee, made my day! :heart_eyes:"));
 
         return fragment;
     }
