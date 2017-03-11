e1f2496b210c16b5db51af6904691bae3830f127
Thomas Richner <thomasrichner@oviva.ch>
2017-03-11 20:54:39 +0100

    Updated map.

--- app/src/main/java/ch/k42/suspendablecoffee/fragments/MapFragment.java
+++ app/src/main/java/ch/k42/suspendablecoffee/fragments/MapFragment.java
@@ -1,32 +1,25 @@
 package ch.k42.suspendablecoffee.fragments;
 
+import android.animation.Animator;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import ch.k42.suspendablecoffee.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MapFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MapFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class MapFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,38 +27,53 @@ public class MapFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
-        MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View fragment = inflater.inflate(R.layout.fragment_map, container, false);
+
+        final ImageView coffeeShopImg = (ImageView) fragment.findViewById(R.id.coffee_shop_img);
+        coffeeShopImg.setVisibility(INVISIBLE);
+
+        final Button button = (Button) fragment.findViewById(R.id.coffee_btn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(coffeeShopImg.getVisibility() == INVISIBLE){
+                    coffeeShopImg.setAlpha(0.0f);
+                    coffeeShopImg.setTranslationY(coffeeShopImg.getHeight());
+                    coffeeShopImg.setVisibility(View.VISIBLE);
+
+                    coffeeShopImg.animate()
+                            .translationY(0)
+                            .alpha(1.0f);
+                }else{
+
+                    coffeeShopImg.animate()
+                            .translationY(coffeeShopImg.getHeight())
+                            .alpha(0.0f)
+                            .withEndAction(new Runnable() {
+                                @Override
+                                public void run() {
+                                    coffeeShopImg.setVisibility(INVISIBLE);
+                                }
+                            });
+                }
+
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        return fragment;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
