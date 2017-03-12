6a0aedade53bf8f1c914d65f6b1112ac16ba15b0
Thomas Richner <thomasrichner@oviva.ch>
2017-03-12 02:12:10 +0100

    Added logos and another image.

--- app/src/main/java/ch/k42/suspendablecoffee/views/TitledImageCardView.java
+++ app/src/main/java/ch/k42/suspendablecoffee/views/TitledImageCardView.java
@@ -2,6 +2,7 @@ package ch.k42.suspendablecoffee.views;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
@@ -14,6 +15,8 @@ public class TitledImageCardView extends LinearLayout {
 
     private TextView titleView;
     private ImageView imageView;
+    private ImageView logoView;
+    private CardView card;
 
     public TitledImageCardView(Context context) {
         super(context);
@@ -53,11 +56,22 @@ public class TitledImageCardView extends LinearLayout {
         this.imageView.setImageResource(resId);
     }
 
+    private int dp2pixel(int dps){
+        final float scale = getContext().getResources().getDisplayMetrics().density;
+        return  (int) (dps * scale + 0.5f);
+    }
+
+    public void setLogoResource(int resId){
+        this.logoView.setImageResource(resId);
+    }
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
 
         titleView = (TextView) findViewById(R.id.titledimagecard_title);
         imageView = (ImageView) findViewById(R.id.titledimagecard_image);
+        logoView = (ImageView) findViewById(R.id.titledimagecard_logo);
+        card = (CardView) findViewById(R.id.titledimagecard_card);
     }
 }
